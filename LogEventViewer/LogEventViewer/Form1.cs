de35702003959936d1038a8fc6159e2d727b1549
space0215@naver.com <space0215@naver.com>
2021-06-23 10:26:27 +0900

    push test

--- LogEventViewer/LogEventViewer/Form1.cs
+++ LogEventViewer/LogEventViewer/Form1.cs
@@ -16,5 +16,10 @@ namespace LogEventViewer
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //20210623 push test
+        }
     }
 }
